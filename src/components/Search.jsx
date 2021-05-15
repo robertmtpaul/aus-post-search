b12b55bb8d06997d2e57f59f8e58b809e2beb0f3
robertmtpaul <rob@glo.id.au>
2021-05-15 11:37:17 +0800

    Added form field for postcode entry which links to handleInput function, storing entry of postcode into state using useState

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,30 @@ import React, {useState} from 'react';
 
 
 const Search = (props) => {
+	//Pass to useState the initial value I want for this state
 	const [ searchPostcode, setSearchPostcode ] = useState('');
 
 	const handleInput = (ev) => {
-		setSearchUsername(ev.target.value);
+		setSearchPostcode(ev.target.value);
 	}
-}
+
+	// const handleSubmit = () => {
+
+	// }
+	// ----------
+	// ---------
+
+	return (
+		<div>
+			<label>Search by Postcode</label>
+			<input type="text" onChange={handleInput} />
+		</div>
+	)
 
 
-export default SearchAddress
+
+} // Search()
+
+
+
+export default Search
