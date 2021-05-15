a69b56720a0aadc720ed441695bf92581eb817c4
robertmtpaul <rob@glo.id.au>
2021-05-15 12:09:07 +0800

    Added suburb field and handleSuburbInput function. Successfully storing both inputs into state.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,26 +3,36 @@ import React, {useState} from 'react';
 
 const Search = (props) => {
 	//Pass to useState the initial value I want for this state
-	const [ searchPostcode, setSearchPostcode ] = useState('');
+	const [ searchPostcode, setSearchPostcode ] = useState('0000');
+	const [ searchSuburb, setSearchSuburb] = useState('suburb');
 
-	const handleInput = (ev) => {
-		setSearchPostcode(ev.target.value);
-		console.log(setSearchPostcode)
+	const handlePostcodeInput = (event) => {
+		setSearchPostcode(event.target.value);
+		console.log(searchPostcode)
+	}
+	const handleSuburbInput = (event) => {
+		setSearchSuburb(event.target.value);
+		console.log(searchSuburb)
 	}
 
-	// const handleSubmit = () => {
+	const handleSubmit = () => {
 
-	// }
-	// ----------
-	// ---------
+	}
 
 	return (
 		<div>
 			<h1>Search address in Australia Post database</h1>
-			<label>Search by Postcode</label>
-			<input type="text" onChange={handleInput} />
+			<form onSubmit={handleSubmit}>
+				<label>Search by postcode</label>
+				<input type="text" onChange={handlePostcodeInput} />
+				<label>Search by suburb</label>
+				<input type="text" onChange={handleSuburbInput} />
+			</form>
 			<p>
 				Typed: { searchPostcode }
+			</p>
+			<p>
+				Typed: { searchSuburb }
 			</p>
 		</div>
 	)
