e41a2a34c1f602070331f326c8d22c12b4126343
robertmtpaul <rob@glo.id.au>
2021-05-15 12:44:05 +0800

    Installed axios, added import statement; added SearchResults component; added API credentials as constants

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 
 const Search = (props) => {
-	//Pass to useState the initial value I want for this state
-	const [ searchPostcode, setSearchPostcode ] = useState('0000');
-	const [ searchSuburb, setSearchSuburb] = useState('suburb');
+  //Pass to useState the initial value I want for this state
+  const [searchPostcode, setSearchPostcode] = useState("");
+  const [searchSuburb, setSearchSuburb] = useState("");
 
-	const handlePostcodeInput = (event) => {
-		setSearchPostcode(event.target.value);
-		console.log(searchPostcode)
-	}
-	const handleSuburbInput = (event) => {
-		setSearchSuburb(event.target.value);
-		console.log(searchSuburb)
-	}
+  const handlePostcodeInput = (event) => {
+    setSearchPostcode(event.target.value);
+    console.log(searchPostcode);
+  };
+  const handleSuburbInput = (event) => {
+    setSearchSuburb(event.target.value);
+    console.log(searchSuburb);
+  };
 
-	const handleSubmit = () => {
+  const handleSubmit = () => {};
 
-	}
+  return (
+    <div>
+      <h2>Search address in Australia Post database</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Search by postcode</label>
+        <input
+          type="text"
+          onChange={handlePostcodeInput}
+          placeholder="e.g. 5000"
+        />
+        <br />
+        {/* TODO: add CSS breaks */}
 
-	return (
-		<div>
-			<h1>Search address in Australia Post database</h1>
-			<form onSubmit={handleSubmit}>
-				<label>Search by postcode</label>
-				<input type="text" onChange={handlePostcodeInput} />
-				<label>Search by suburb</label>
-				<input type="text" onChange={handleSuburbInput} />
-			</form>
-			<p>
-				Typed: { searchPostcode }
-			</p>
-			<p>
-				Typed: { searchSuburb }
-			</p>
-		</div>
-	)
+        <label>Search by suburb</label>
+        <input
+          type="text"
+          onChange={handleSuburbInput}
+          placeholder="e.g. Smithfield"
+        />
+        <br />
+        {/* TODO: add CSS breaks */}
+        <button onClick={handleSubmit}>Search address</button>
+      </form>
+      {/* TODO: remove below code */}
+      <h3>Tests:</h3>
+      <p>Typed: {searchPostcode}</p>
+      <p>Typed: {searchSuburb}</p>
+    </div>
+  );
+}; // Search()
 
-
-} // Search()
-
-
-
-export default Search
+export default Search;
---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,4 @@
+import React, {useState, useEffect} from 'react';
+
+const AUPOST_BASE_URL = "https://digitalapi.auspost.com.au";
+const AUPOST_KEY = "872608e3-4530-4c6a-a369-052accb03ca8"
